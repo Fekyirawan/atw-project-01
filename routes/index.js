d1bc4e5600b23071fc3aa5361578f5e2679e02ba
Fekyirawan <71133960+Fekyirawan@users.noreply.github.com>
2021-03-17 21:56:42 +0700

    atw-project-01

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TEKNIK INFORMATIKA' });
+  res.render('index', { title: 'MAHASISWA TEKNIK INFORMATIKA' });
 });
 
 router.get('/about', function(req, res, next) {
